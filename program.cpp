0af2207ccd5c8547d6a7227d2be2c14923bc30b1
b9cv9 <emil.kurakin95@mail.ru>
2023-04-03 22:21:08 +0400

    Исправление программы под другую последовательность Программа исправленна так, чтобы последовательность чисел начиналась со значений (0, 1, ...).  main

--- program.cpp
+++ program.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include<iostream>
 using namespace std;
 
-int fib(int n){
+int fib(int n)
+{
     if(n == 0) return 0;
     if(n == 1 || n == 2) return 1;
-    return fib(n - 1)+fib(n - 2);
+    if(n < 2) return 0;
+    if(n == 2 || n == 3) return 1;
+    return fib(n-1)+fib(n-2);
 }
 
 int main(){
-    cout << fib(10) << endl;
+    cout<<fib(10) << endl;
 }
